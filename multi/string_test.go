88ef59065fe1b23418f6709badd247262b581df0
whosonfirst <whosonfirst@localhost>
2022-09-07 10:31:35 -0700

    add MultiCSVString

--- multi/string_test.go
+++ multi/string_test.go
@@ -1,9 +1,24 @@
 package multi
 
 import (
+	"flag"
 	"testing"
 )
 
 func TestMultiString(t *testing.T) {
 	t.Skip()
+}
+
+func TestMultiCSVString(t *testing.T) {
+
+	var m MultiCSVString
+
+	fs := flag.NewFlagSet("test", flag.ExitOnError)
+	fs.Var(&m, "test", "...")
+
+	fs.Set("test", "hello,world")
+
+	if len(m) != 2 {
+		t.Fatalf("Unexpected length: %d", len(m))
+	}
 }
