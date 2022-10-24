f2a9506fbdc0a33012c283892391511e14962220
whosonfirst <whosonfirst@localhost>
2022-10-24 13:12:27 -0700

    add multi.KeyValueCSVString type

--- multi/keyvalue_test.go
+++ multi/keyvalue_test.go
@@ -1,9 +1,29 @@
 package multi
 
 import (
+	"flag"
 	"testing"
 )
 
 func TestMultiKeyValuet(t *testing.T) {
 	t.Skip()
+}
+
+func TestKeyValueCSVString(t *testing.T) {
+
+	pairs := "hello=world,foo=bar"
+	var kv KeyValueCSVString
+
+	fs := flag.NewFlagSet("test", flag.ExitOnError)
+	fs.Var(&kv, "test", "...")
+
+	fs.Set("test", pairs)
+
+	if len(kv) != 2 {
+		t.Fatalf("Unexpected length, %d", len(kv))
+	}
+
+	if kv.String() != pairs {
+		t.Fatalf("Unexpected string for pairs: %s", kv.String())
+	}
 }
