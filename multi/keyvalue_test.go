d3c3cafb7ebb4d77094e569a0840b99b9957171a
Aaron Straup Cope <thisisaaronland@users.noreply.github.com>
2025-11-04 12:40:05 -0800

    Update `multi.KeyValueStringFlag` to implement `Set` and `String` methods. (#2)
    * implement Set method for KeyValueStringFlag
    
    * implement String for KeyValueStringFlag
    
    ---------
    
    Co-authored-by: thisisaaronland <thisisaaronland@localhost>

--- multi/keyvalue_test.go
+++ multi/keyvalue_test.go
@@ -6,7 +6,39 @@ import (
 )
 
 func TestMultiKeyValuet(t *testing.T) {
-	t.Skip()
+
+	kv := new(KeyValueStringFlag)
+	err := kv.Set("foo=bar")
+
+	if err != nil {
+		t.Fatalf("Failed to set KeyValueStringFlag, %v", err)
+	}
+
+	v := kv.Value().(string)
+
+	if v != "bar" {
+		t.Fatalf("Invalid value for KeyValueStringFlag")
+	}
+
+	multi_kv := new(KeyValueString)
+	err = multi_kv.Set("bar=foo")
+
+	if err != nil {
+		t.Fatalf("Failed to set multi KeyValueString, %v", err)
+	}
+
+	v2 := multi_kv.Get().(KeyValueString)
+
+	if len(v2) != 1 {
+		t.Fatal("Invalid count for multi KeyValueString")
+	}
+
+	multi_v := v2[0].Value().(string)
+
+	if multi_v != "foo" {
+		t.Fatalf("Invalid value for multi KeyValueString")
+	}
+
 }
 
 func TestKeyValueCSVString(t *testing.T) {
